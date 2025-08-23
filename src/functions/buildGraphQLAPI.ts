6ed6ae5a795e9690e21ecd53b23186e2bf8d2828
Jannael Orlando <orlandojannael@gmail.com>
2025-08-23 16:46:53 -0600

    non-optional fields

--- src/functions/buildGraphQLAPI.ts
+++ src/functions/buildGraphQLAPI.ts
@@ -1,11 +1,14 @@
 function convertTsInterfaceToGraphQLType (tsInterface: string): string {
   tsInterface = tsInterface.replace('interface', 'type')
-  tsInterface = tsInterface.replace(/ string/g, ' String')
   tsInterface = tsInterface.replace('number', ' Int')
-  tsInterface = tsInterface.replace('boolean', ' Boolean')
+
+  tsInterface = tsInterface.replace(/ string/g, ' String')
+  tsInterface = tsInterface.replace(/ boolean/g, ' Boolean')
 
   // now lets check for arrays we want to change string[] => [string]
   tsInterface = tsInterface.replace(/\b(\w+)\[\]/g, (_, type: string) => `[${type}]`)
+
+  // field?: string => field: string!
   tsInterface.matchAll(/\b\w+:\s*\[?\w+\]?/g)
     .forEach(m => {
       tsInterface = tsInterface.replace(m[0], `${m[0]}!`)
