8f72f60fd685e86bb3afd9a10942d59b10919f0d
Jannael Orlando <orlandojannael@gmail.com>
2025-08-23 16:00:34 -0600

    change tsc-interfaces to graphQL-types

---
+++ src/functions/buildGraphQLAPI.ts
@@ -0,0 +1,28 @@
+function convertTsInterfaceToGraphQLType (tsInterface: string): string {
+  tsInterface = tsInterface.replace('interface', 'type')
+  tsInterface = tsInterface.replace(/ string/g, ' String')
+  tsInterface = tsInterface.replace('number', ' Int')
+  tsInterface = tsInterface.replace('boolean', ' Boolean')
+
+  // now lets check for arrays we want to change string[] => [string]
+  tsInterface = tsInterface.replace(/\b(\w+)\[\]/g, (_, type: string) => `[${type}]`)
+  tsInterface.matchAll(/\b\w+:\s*\[?\w+\]?/g)
+    .forEach(m => {
+      tsInterface = tsInterface.replace(m[0], `${m[0]}!`)
+    })
+
+  tsInterface = tsInterface.replace('?', '')
+  return tsInterface
+}
+
+const tsInterface = `
+interface User {
+  id: number
+  name: string
+  email?: string
+  tags: string[]
+  isActive: boolean
+}
+`
+
+console.log(convertTsInterfaceToGraphQLType(tsInterface))
